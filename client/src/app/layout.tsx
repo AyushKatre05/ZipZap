4dd6f6c686880f0227ae41fdf4fdc62f6e5f5b7b
Ayush Katre <ayushkatre1801@gmail.com>
2025-03-17 17:18:31 +0530

    added common layout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "sonner";
+import { Toaster } from "@/components/ui/toaster";
+import CommonLayout from "@/components/common/layout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <CommonLayout>{children}</CommonLayout>
         <Toaster />
       </body>
     </html>
